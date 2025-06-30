5fabdd172ed8e6dc563ec6f5609b843ab1fda18f
subhan2108 <sk1623969@gmail.com>
2025-06-30 03:06:58 -0400

    implemeted message when you are outbid

--- auction_core/auctions/consumers.py
+++ auction_core/auctions/consumers.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 from .models import Auction, AuctionItem
 
 User = get_user_model()
@@ -12,25 +13,31 @@ class AuctionConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.item_id = self.scope['url_route']['kwargs']['item_id']
         self.room_group_name = f'auction_{self.item_id}'
+        self.user = self.scope["user"]
+
+        if self.user.is_authenticated:
+            self.user_group_name = f"user_{self.user.id}"
+            await self.channel_layer.group_add(self.user_group_name, self.channel_name)
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
-        # Send current status immediately on connect
+        # Send current auction status immediately
         status = await self.get_auction_status()
         await self.send(text_data=json.dumps({'status': status}))
 
-        # Start periodic status check
         self.check_status_task = asyncio.create_task(self.check_auction_status())
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        if self.user.is_authenticated:
+            await self.channel_layer.group_discard(self.user_group_name, self.channel_name)
         self.check_status_task.cancel()
 
     async def receive(self, text_data):
         data = json.loads(text_data)
         bid = float(data.get('bid', 0))
-        user = self.scope["user"]
+        user = self.user
 
         if not user.is_authenticated:
             await self.send(text_data=json.dumps({
@@ -39,8 +46,7 @@ class AuctionConsumer(AsyncWebsocketConsumer):
             return
 
         real_user = await self.get_user(user.id)
-
-        success = await self.update_bid(bid, real_user)
+        success, outbid_user_id = await self.update_bid(bid, real_user)
 
         if success:
             await self.channel_layer.group_send(
@@ -48,9 +54,19 @@ class AuctionConsumer(AsyncWebsocketConsumer):
                 {
                     'type': 'auction_bid',
                     'bid': bid,
-                    'user': real_user.username
+                    'user': real_user.username,
                 }
             )
+
+            # Notify the previous highest bidder
+            if outbid_user_id and outbid_user_id != real_user.id:
+                await self.channel_layer.group_send(
+                    f"user_{outbid_user_id}",
+                    {
+                        'type': 'outbid_notification',
+                        'message': f"You've been outbid on item #{self.item_id}."
+                    }
+                )
         else:
             await self.send(text_data=json.dumps({
                 'error': 'Invalid bid - must be higher than current bid or auction is closed.'
@@ -67,11 +83,15 @@ class AuctionConsumer(AsyncWebsocketConsumer):
             'status': event['status']
         }))
 
+    async def outbid_notification(self, event):
+        await self.send(text_data=json.dumps({
+            'notification': event['message']
+        }))
+
     async def check_auction_status(self):
         last_status = None
         while True:
             current_status = await self.get_auction_status()
-
             if current_status != last_status:
                 await self.channel_layer.group_send(
                     self.room_group_name,
@@ -84,14 +104,13 @@ class AuctionConsumer(AsyncWebsocketConsumer):
 
             if current_status == "closed":
                 break
-
             await asyncio.sleep(5)
 
     @database_sync_to_async
     def get_auction_status(self):
         try:
             auction = Auction.objects.get(product_id=self.item_id)
-            if auction.end_time and auction.end_time <= datetime.now():
+            if auction.end_time and auction.end_time <= timezone.now():
                 if auction.status != 'closed':
                     auction.status = 'closed'
                     auction.save()
@@ -107,15 +126,15 @@ class AuctionConsumer(AsyncWebsocketConsumer):
     @database_sync_to_async
     def update_bid(self, new_bid, user):
         try:
-            item = AuctionItem.objects.get(pk=self.item_id)
             auction = Auction.objects.get(product_id=self.item_id)
             if auction.status == 'closed':
-                return False
+                return False, None
             if new_bid > float(auction.current_bid):
+                outbid_user_id = auction.bidder.id if auction.bidder else None
                 auction.current_bid = new_bid
                 auction.bidder = user
                 auction.save()
-                return True
-        except (AuctionItem.DoesNotExist, Auction.DoesNotExist):
+                return True, outbid_user_id
+        except (Auction.DoesNotExist, AuctionItem.DoesNotExist):
             pass
-        return False
+        return False, None
